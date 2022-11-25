54388d47328f602e39755e5687a3426ae8d33c00
Nathalie <xnhogenbergx@gmail.com>
2022-11-25 14:48:15 -0800

    commenting code
    plus prefab made for letter and updating the player prefab

--- Assets/Letter.cs
+++ Assets/Letter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Letter : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     public void Dissapear()
     {
         Destroy(gameObject);
--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -8,16 +8,10 @@ public class PickUp : MonoBehaviour
     RaycastHit hit;
     public float range;
 
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        //raycast out. from the player, hit is information gather from the thing that it hits, forwards direction, how far.
         Debug.DrawRay(transform.position, hit.point, Color.yellow);
         if (Physics.Raycast(transform.position, transform.forward, out hit, range))
         {
@@ -27,21 +21,26 @@ public class PickUp : MonoBehaviour
 
     void Selecting()
     {
-
+        //get the infro from hit object, save it 
         var selection = hit.transform;
+        //get the material
         var selectionRenderer = selection.GetComponent<Renderer>();
         if (selectionRenderer != null)
         {
+            //change the material to the highlighted material
             selectionRenderer.material = highlightMaterial;
         }
 
         if (Input.GetKey("space"))
         {
+            //name of the object you have selected
             Debug.Log(hit.transform.name);
 
+            //momentairily link the letter to the player. 
             Letter letter = hit.transform.GetComponent<Letter>();
             if (letter != null)
             {
+                //call upon the dystroy function of the letter
                 letter.Dissapear();
             }
         }
