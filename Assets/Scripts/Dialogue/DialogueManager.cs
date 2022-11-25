88c8473224902356b476e04008f3ba39d7959c4f
Nathalie <xnhogenbergx@gmail.com>
2022-11-24 16:32:54 -0800

    start dialog
    textmesh pro
    neccecery scripts
    stuff n shit

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
