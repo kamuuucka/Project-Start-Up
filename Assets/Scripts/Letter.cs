b26dfd56fcac3eb6cccf4481a43dfd1eda0e2ca3
Nathalie <xnhogenbergx@gmail.com>
2022-11-29 09:48:26 -0800

    pickup works
    with colour change
    name is printed when selecting
    dissapears when selecting

--- Assets/Scripts/Letter.cs
+++ Assets/Scripts/Letter.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 
 public class Letter : MonoBehaviour
 {
+    public Material[] materials;
+    Renderer renderers;
 
-    public void Dissapear()
+    private void Start()
     {
-        Destroy(gameObject);
+        renderers = GetComponent<Renderer>();
+        renderers.enabled = true;
+        renderers.sharedMaterial = materials[0];
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Selecting();
+            Debug.Log("now");
+
+            renderers.sharedMaterial = materials[1];
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            renderers.sharedMaterial = materials[0];
+        }
+    }
+
+    void Selecting()
+    {
+        //when pressing space
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log(gameObject.name);
+            Destroy(gameObject);
+        }
     }
 }
