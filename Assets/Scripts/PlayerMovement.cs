bd7aeccb34f39a1d853daf9812b62b6ebc0a2e64
Nathalie <xnhogenbergx@gmail.com>
2022-12-02 16:00:46 -0800

    kidding
    but the setup is in place for monday

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+
     Rigidbody rb;
-    // Start is called before the first frame update
+    Animator animator;
+
+    int isWalkingHash;
+
     void Start()
     {
+        animator= GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
     }
 
-    // Update is called once per frame
     void FixedUpdate() // FIX!
     {
+        bool isWalking  = animator.GetBool(isWalkingHash);
+
         float xMove = Input.GetAxisRaw("Horizontal");
         float zMove = Input.GetAxisRaw("Vertical");
 
         rb.velocity = new Vector3(xMove, 0, zMove) * speed + new Vector3(0, rb.velocity.y, 0); // FiX!
+    
+/*        if() // put in the when moving thing
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }*/
     }
 }
