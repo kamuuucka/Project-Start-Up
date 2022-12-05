c17de6ac2f4c70ced4884ccda18acc3bc86ecfaa
Nathalie <xnhogenbergx@gmail.com>
2022-12-05 12:18:21 -0800

    Revert "Merge branch 'Sounds' into develop"
    This reverts commit 25a78ebf38cb06caaa59d151d4d29178afe1361e.

--- Assets/Scripts/LetterManager.cs
+++ Assets/Scripts/LetterManager.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LetterManager : MonoBehaviour
 {
+
+    [SerializeField] private Timer timer;
+
     public GameObject[] letterPlacement = new GameObject[9];
     public List<string> correctAnswers = new List<string>();
 
@@ -14,42 +18,36 @@ public class LetterManager : MonoBehaviour
     private int answerNumber = 0;
     private string wordToCheck = "";
 
-    //new
-    public GameObject Correct;
-    public GameObject Wrong;
-
     void Awake()
     {
-        levelData = GetComponent<LettersAreaData>();         
+        timer.SetBool(false);
+        levelData = GetComponent<LettersAreaData>();
     }
 
     private void Update()
     {
+
         if (wordToCheck.Equals(correctAnswers[answerNumber]))
         {
-            
-
             if (Input.GetKeyUp(KeyCode.Return))
             {
-                if(answerNumber == 4)
+                if (answerNumber == 4)
                 {
-                    SceneManager.LoadScene(0);
+                    timer.SetBool(true);
+                    Debug.Log(timer.GetTime());
+
+                    SceneManager.LoadScene(3);
                 }
                 wordToCheck = "";
                 levelData.SetLevelChange(true);
                 amount = 0;
                 answerNumber++;
-                //new
-                Correct.SetActive(true);
-                Correct.SetActive(false);
             }
         }
         else
         {
             if (Input.GetKeyUp(KeyCode.Return))
             {
-                Wrong.SetActive(true);
-                Wrong.SetActive(false);
                 LevelReload();
             }
         }
@@ -74,7 +72,6 @@ public class LetterManager : MonoBehaviour
 
     public void LevelReload()
     {
-        Correct.SetActive(false);
         levelData.SetLevelReload(true);
         amount = 0;
         wordToCheck = "";
